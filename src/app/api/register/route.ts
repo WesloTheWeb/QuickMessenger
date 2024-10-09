54360a6c6b741dee0e20bb089d96bab5449fddf1
Wesley <wesleyadwebster@gmail.com>
2024-10-09 17:37:14 -0500

    Registration route made, form clears and added server side validation and notes

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -4,15 +4,28 @@ import User from '@/models/User';
 
 export async function POST(request: Request) {
   try {
+    // ? Connects to database
     await connectToDatabase();
-    
+
+    // ? Extracts data from the request
     const { firstName, lastName, gender, age, country, email, userName, password } = await request.json();
 
+    // ! Server-side input validation:
+    if (typeof firstName !== 'string' || firstName.length > 50) {
+      return NextResponse.json({ message: 'Invalid first name' }, { status: 400 });
+    };
+
+    if (!['male', 'female', 'other'].includes(gender)) {
+      return NextResponse.json({ message: 'Invalid gender' }, { status: 400 });
+    }
+
+    // ? Check for existing user:
     const existingUser = await User.findOne({ $or: [{ email }, { userName }] });
     if (existingUser) {
       return NextResponse.json({ message: 'Email or username already exists' }, { status: 400 });
     }
 
+    // ? Creating a new user:
     const newUser = new User({
       firstName,
       lastName,
@@ -26,6 +39,7 @@ export async function POST(request: Request) {
 
     await newUser.save();
 
+    // ? Sends a response back to client:
     return NextResponse.json({ message: 'User created successfully' }, { status: 201 });
   } catch (error) {
     console.error('Registration error:', error);
