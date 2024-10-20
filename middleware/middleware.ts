b9c5db36df7da7cfc3fe3028d77c74a0f5179cf1
Wesley <wesleyadwebster@gmail.com>
2024-10-19 19:57:18 -0500

    Made the Dashboard top level page a server component with getUser utility function

--- middleware/middleware.ts
+++ middleware/middleware.ts
@@ -1,21 +1,22 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { jwtVerify } from 'jose';
+import jwt from 'jsonwebtoken';
 
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value;
 
   if (!token) {
     return NextResponse.redirect(new URL('/login', request.url));
-  };
+  }
 
   try {
-    const { payload } = await jwtVerify(token, new TextEncoder().encode(process.env.JWT_SECRET));
+    // Using jsonwebtoken instead of jose
+    jwt.verify(token, process.env.JWT_SECRET as string);
     return NextResponse.next();
   } catch (error) {
     return NextResponse.redirect(new URL('/login', request.url));
-  };
-};
+  }
+}
 
 export const config = {
   matcher: ['/dashboard/:path*', '/profile/:path*', '/api/protected/:path*'],
